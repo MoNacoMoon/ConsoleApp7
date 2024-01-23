f9c0e498b482a8acfc70ca79a201a6daf49617ed
EvoL_ <EvoL_@MOON>
2024-01-23 20:58:07 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 1;
+            //while (x <= 10)
+            //{
+            // Console.WriteLine(x);
+            // x++;
+            //}
+
+
+
+            //int x = 1;
+            //do
+            //{
+            // Console.WriteLine(x);
+            // x++;
+            //}
+            //while (x <= 10);
+
+
+            //for (int x = 1; x <=10; x++)
+            //{
+            // Console.WriteLine(x);
+            //}
+
+            //Console.WriteLine("Введите целое число");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int x = 1; x <= n; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //int x = 1;
+            //while (x <= 5)
+            //{
+            //    Console.WriteLine("Привет!");
+            //    x++;
+            //}
+
+
+
+
+            //int x = 1;
+            //Console.WriteLine("");
+            //for (int i = 1; x <= 18; i++)
+            //{
+            //    Console.ForegroundColor = (ConsoleColor)i;
+            //    Console.Write("1"); x++;
+            //    if (i == 15) i = 1;
+            //}
+
+
+            //int n = 1;
+            //Console.WriteLine("");
+            //for (int i = 1; n <= 25; i++)
+            //{
+            //    Console.ForegroundColor = (ConsoleColor)i;
+            //    Console.WriteLine("1"); n++;
+            //    if (i == 15) i = 1;
+            //}
+            //Console.ReadKey();
+
+        }
+    }
+}
+
+            
+
+
+            
+        
+    
+
